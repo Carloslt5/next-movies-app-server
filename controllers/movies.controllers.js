5c1239336aa86f43727b110e70d1a2d9c2b58312
Carloslt <carloslt.proyectos@gmail.com>
2023-07-13 18:27:40 +0200

    add limit and skip

--- controllers/movies.controllers.js
+++ controllers/movies.controllers.js
@@ -2,8 +2,15 @@ const Movie = require("../models/Movie.models")
 
 const getMovies = async (req, res, next) => {
 
+    let { limit, skip } = req.query
+
+    limit = limit ?? 10
+    skip = skip ?? 0
+
+
+    console.log({ limit, skip })
     try {
-        const movie = await Movie.find().limit(500).sort({ title: 1 })
+        const movie = await Movie.find().skip(skip).limit(limit).sort({ title: 1 })
         return res.status(200).json(movie);
     } catch (error) {
         return res.status(400).json({ success: false, error });
